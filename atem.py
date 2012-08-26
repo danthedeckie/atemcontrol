febc7cd8db13882f8b9501e59b120086f7da937a
danthedeckie <danthedeckie@gmail.com>
2012-08-26 16:47:20 +0000

    Slowly heading for pep8, clean ups, etc. testing on Pi.

--- atem.py
+++ atem.py
@@ -15,7 +15,7 @@
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http: //www.gnu.org/licenses/>.
 
 from struct import pack, unpack
 from binascii import hexlify, unhexlify
@@ -27,8 +27,10 @@ import os
 from time import sleep
 import logging
 
+
 def rand(max):
-    return int(rand_uniform(0,max))
+    return int(rand_uniform(0, max))
+
 
 class ATEMController(object):
 
@@ -38,7 +40,7 @@ class ATEMController(object):
     hello_finished = False
 
     def __init__(self, ip, port):
-        self.connect(ip,port)
+        self.connect(ip, port)
 
     def connect(self, ip, port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -46,16 +48,16 @@ class ATEMController(object):
         self.uid = self.send_hello()
         #self.sock.send(unhexlify("1014439d00000000000000000100000000000000"))
         #800c67a50000000000500000
-        logging.info( self.uid )
+        logging.info(self.uid)
         self.sock.setblocking(False)
 
     def send_hello(self):
-        self.uid   = (((rand(254) + 1) << 8) + rand(254) + 1) & 0x7FFF
+        self.uid = (((rand(254) + 1) << 8) + rand(254) + 1) & 0x7FFF
         #self.uid = 0x67a5
-        data  = pack("!HHHH", 0x0100, 0x0000, 0x0000, 0x0000)
-        logging.info( "Data" + str(hexlify(data)))
+        data = pack("!HHHH", 0x0100, 0x0000, 0x0000, 0x0000)
+        logging.info("Data" + str(hexlify(data)))
         hello = pack("!BBHHHHH", 0x10, 0x14, self.uid, 0, 0, 0x0000, 0) + data
-        logging.info( "Hello:" + str(hexlify(hello)) )
+        logging.info("Hello: " + str(hexlify(hello)))
         #101454b200000000000000000100000000000000
 
         self.sock.send(hello)
@@ -66,13 +68,13 @@ class ATEMController(object):
         cmd += ((ln >> 8) & 0x07)
         pkt = pack("!BBHHHHH", cmd, ln, self.uid, cout, un1, un2, cin) + payload
         #if not ln==12:
-        #    print "Send:", hexlify(pkt)
+        #    print "Send: ", hexlify(pkt)
         #    print_pkt(cmd, ln, self.uid, cout, un1, un2, cin, payload)
         self.sock.send(pkt)
 
     def recv_pkt(self, data):
         pkt = data
-        cmd, len, self.uid, self.count_out, unknown1, unknown2, self.count_in = unpack("!BBHHHHH", data[0:12])
+        cmd, len, self.uid, self.count_out, unknown1, unknown2, self.count_in = unpack("!BBHHHHH", data[0: 12])
         payload = data[12:]
         #(port, ipaddr) = self.sockaddr_in($self.sock->peername)
         len = ((cmd & 0x07) << 8) + len
@@ -80,8 +82,8 @@ class ATEMController(object):
         return (cmd, len, self.count_out, unknown1, unknown2, self.count_in, payload)
 
     def print_pkt(self, cmd, len, count_out, unknown1, unknown2, count_in, payload):
-        print ("Cmd:", hex(cmd), 
-               "Len:", len, 
+        print ("Cmd:", hex(cmd),
+               "Len:", len,
                "Uid:", hex(self.uid),
                "Unkn1:", unknown1,
                "Unkn2:", unknown2,
@@ -97,18 +99,18 @@ class ATEMController(object):
         if not data:
             print "Nodat"
             return False
-        #print "Recv:", hexlify(data[0:16])
+        #print "Recv: ", hexlify(data[0: 16])
         args = self.recv_pkt(data)
         #print_pkt(*args)
         cmd, ln, self.count_out, unknown1, unknown2, self.count_in, payload = args
-        if not ln==12:
+        if not ln == 12:
             logging.info("R")
             print_pkt(*args)
         if cmd & 0x10:
             # hello response
             #undef, new_self.uid, undef, undef = unpack("!HHHH", payload)
             #self.uid = unpack("!HHHH", payload)[1]
-            logging.info('|'.join(["Helloresp", self.uid, cmd, cmd & 0x10]) )
+            logging.info('|'.join(["Helloresp", self.uid, cmd, cmd & 0x10]))
             #send_pkt(0x80, self.uid, 0x0, 0, 0x00e9, 0, '')
             self.send_pkt(0x80, 0, 0, 0x0050, 0, '')
             return True
@@ -116,46 +118,45 @@ class ATEMController(object):
             #print "G8p", self.count_in, hello_finished
             if self.count_in == 0x04 and not self.hello_finished:
                 self.hello_finished = True
-                self.mycount+=1
-                logging.info( "Hellofinish" )
-            if hello_finished:
+                self.mycount += 1
+                logging.info("Hellofinish")
+            if self.hello_finished:
                 #print "SHF"
                 self.send_pkt(0x80, self.count_in, 0, 0, 0, '')
                 self.send_pkt(0x08, 0, 0, 0, self.mycount, '')
-                self.mycount+=1
-    #else:
-    #   self.send_pkt(0x80, 0, 0, self.mycount, 0, '')
-    #   self.mycount+=1
+                self.mycount += 1
+        #else:
+        #   self.send_pkt(0x80, 0, 0, self.mycount, 0, '')
+        #   self.mycount+=1
 
         #Sett standard command number
         if (self.mycount == 0 and self.hello_finished):
             cmd = 0x80
 
-
     # Higher level functions:
     # TODO: figure out proper names.
 
     def fade(self, amount): #amount 1-1000
-        payload = pack("!HHHHHH", 0x000c, 0x0000,0x4354, 0x5073, 0x0054, amount)#value from 0-1000
+        payload = pack("!HHHHHH", 0x000c, 0x0000, 0x4354, 0x5073, 0x0054, amount)#value from 0-1000
         self.send_pkt(0x88, self.count_in, 0, 0, self.mycount, payload)
-        logging.info( "SENDFADE" )
+        logging.info("SENDFADE")
 
     def set_pgm_bus(self, channel):
         payload = unhexlify("000c00004350674900")+chr(channel)+unhexlify("0000")
         self.send_pkt(0x08, self.count_in, 0, 0, self.mycount, payload)
-        logging.info( "SENDTPKG" )
+        logging.info("SENDTPKG")
 
     def set_pvw_bus(self, channel):
         payload = unhexlify("000c00004350764900")+chr(channel)+unhexlify("0000")
         #payload = unhexlify("000c00004350764900060000")
         self.send_pkt(0x08, self.count_in, 0, 0, self.mycount, payload)
-        logging.info( "SENDBPKG" )
+        logging.info("SENDBPKG")
 
     def auto_fade(self):
         payload = unhexlify("000c97024441757400000000")
         #payload = unhexlify("000c00004350764900060000")
         atem.send_pkt(0x08, self.count_in, 0, 0, self.mycount, payload)
-        logging.info( "SENDATPKG" )
+        logging.info("SENDATPKG")
 
     def close(self):
         self.sock.close()
